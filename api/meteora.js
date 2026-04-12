39a02ced47e72b5fedd2eb0191e5c96b54617967
nftswanny-web <nft.swanny@gmail.com>
2026-04-12 15:23:03 +0200

    Update meteora.js

--- api/meteora.js
+++ api/meteora.js
@@ -1,10 +1,11 @@
 const fetch = require('node-fetch');
+const { applyCors, rateLimit } = require('./_utils');
 
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', '*');
+  applyCors(req, res);
   if (req.method === 'OPTIONS') return res.status(200).end();
+  if (req.method !== 'GET') return res.status(405).json({ error: 'method_not_allowed' });
+  if (rateLimit(req, res, 'meteora', 60 * 1000, 30)) return;
 
   try {
     const path = req.query.path || '/pools';
