a3a9db5f1534b670cf6d1f4fb71b9b37f660d5e3
nftswanny-web <nft.swanny@gmail.com>
2026-04-12 15:21:43 +0200

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,8 +1,10 @@
+const { applyCors, rateLimit } = require('./_utils');
+
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', '*');
+  applyCors(req, res);
   if (req.method === 'OPTIONS') return res.status(200).end();
+  if (req.method !== 'GET') return res.status(405).json({ error: 'method_not_allowed' });
+  if (rateLimit(req, res, 'config', 60 * 1000, 60)) return;
 
   return res.status(200).json({
     googleClientId: process.env.GOOGLE_CLIENT_ID || process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '',
